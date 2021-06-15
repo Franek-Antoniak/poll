8087b6eef0f27796ef6bcd74b1da6c90d1c31484
Franek-Antoniak <31598277+Franek-Antoniak@users.noreply.github.com>
2021-06-15 22:16:29 +0200

    Changing strength of BCryptPasswordEncoder

--- src/main/java/com/staszic/poll/security/SecurityConfig.java
+++ src/main/java/com/staszic/poll/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(4);
     }
 
 
