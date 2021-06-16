f67ac18caa0e08e5b3fa0cd95adad028fb8d5f16
Franek-Antoniak <31598277+Franek-Antoniak@users.noreply.github.com>
2021-06-16 14:28:03 +0200

    Adding more features to cleaner - deleting all Images being storage in images folder. Max size of files sent to the server changed to 5 MB.

--- src/main/java/com/staszic/poll/cleaner/CleanerService.java
+++ src/main/java/com/staszic/poll/cleaner/CleanerService.java
@@ -5,6 +5,8 @@ import com.staszic.poll.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+
 @Service
 @RequiredArgsConstructor
 public class CleanerService {
@@ -16,5 +18,14 @@ public class CleanerService {
     public void clearDataBase() {
         userRepository.deleteAll();
         imageRepository.deleteAll();
+        File directory = new File("images");
+
+        File[] files = directory.listFiles();
+        assert files != null;
+        for (File file: files) {
+            if(!file.delete())
+                System.out.println("Failed to delete " + file.getName());
+        }
+        System.out.println("Files from: " + directory.getName() + "deleted");
     }
 }
